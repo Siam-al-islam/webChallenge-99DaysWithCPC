099f6119243a7ea735ad5b774f6deb036581d195
Siam-al-islam <siamalislam692@gmail.com>
2024-11-01 19:05:20 +0600

    first commit

---
+++ day1/script.js
@@ -0,0 +1,29 @@
+const yellowBtn = document.getElementById("yellow-btn");
+const blueBtn = document.getElementById("blue-btn");
+const redBtn = document.getElementById("red-btn");
+const greenBtn = document.getElementById("green-btn");
+const blackBtn = document.getElementById("black-btn");
+const purpleBtn = document.getElementById("purple-btn");
+const navyBtn = document.getElementById("navy-btn");
+
+yellowBtn.addEventListener('click', function () {
+    document.body.style.backgroundColor = 'yellow';
+})
+blueBtn.addEventListener('click', function () {
+    document.body.style.backgroundColor = 'blue';
+})
+redBtn.addEventListener('click', function () {
+    document.body.style.backgroundColor = 'red';
+})
+greenBtn.addEventListener('click', function () {
+    document.body.style.backgroundColor = 'green';
+})
+blackBtn.addEventListener('click', function () {
+    document.body.style.backgroundColor = 'black';
+})
+purpleBtn.addEventListener('click', function () {
+    document.body.style.backgroundColor = 'purple';
+})
+navyBtn.addEventListener('click', function () {
+    document.body.style.backgroundColor = 'navy';
+})
