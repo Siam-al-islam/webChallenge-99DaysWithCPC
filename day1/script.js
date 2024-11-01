86600edc248d6d3c5d3b351c80d98e41aaec3796
Siam-al-islam <siamalislam692@gmail.com>
2024-11-01 19:17:58 +0600

    try with seperate function

--- day1/script.js
+++ day1/script.js
@@ -6,24 +6,32 @@ const blackBtn = document.getElementById("black-btn");
 const purpleBtn = document.getElementById("purple-btn");
 const navyBtn = document.getElementById("navy-btn");
 
-yellowBtn.addEventListener('click', function () {
-    document.body.style.backgroundColor = 'yellow';
-})
+const yellowBg = () => {
+    document.body.style.backgroundColor = "yellow";
+}
+
+yellowBtn.addEventListener('click', yellowBg)
+
 blueBtn.addEventListener('click', function () {
     document.body.style.backgroundColor = 'blue';
 })
+
 redBtn.addEventListener('click', function () {
     document.body.style.backgroundColor = 'red';
 })
+
 greenBtn.addEventListener('click', function () {
     document.body.style.backgroundColor = 'green';
 })
+
 blackBtn.addEventListener('click', function () {
     document.body.style.backgroundColor = 'black';
 })
+
 purpleBtn.addEventListener('click', function () {
     document.body.style.backgroundColor = 'purple';
 })
+
 navyBtn.addEventListener('click', function () {
     document.body.style.backgroundColor = 'navy';
 })
