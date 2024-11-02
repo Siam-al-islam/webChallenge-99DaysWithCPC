b95c6074de2d3bf3f90b34330ace5d6fada5cb42
Siam-al-islam <siamalislam692@gmail.com>
2024-11-02 18:30:57 +0600

    added color generator function

---
+++ day2/script.js
@@ -0,0 +1,15 @@
+const generateBtn = document.getElementById('btn');
+
+const getRandomColor = () => {
+    let letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+const setRandomColor = () => {
+    document.body.style.backgroundColor = getRandomColor();
+    console.log(getRandomColor())
+}
