8a20930a8d47b09c6480bc8df4557649c07353b6
Siam-al-islam <siamalislam692@gmail.com>
2024-11-05 21:48:18 +0600

    design added

---
+++ day5/script.js
@@ -0,0 +1,23 @@
+const inputField = document.querySelector('#input-field');
+const form = document.getElementById('form');
+const addBtn = document.querySelector('#add-btn');
+const itemContainer = document.querySelector('#list-container');
+const item = document.querySelector('.item');
+
+function handleForm(event) { event.preventDefault(); }
+form.addEventListener('submit', handleForm);
+
+const addList = () => {
+    if (inputField.value == "") {
+        alert("please add something");
+    }
+    else {
+        const newItem = document.createElement('p');
+        newItem.innerText = inputField.value;
+        itemContainer.appendChild(newItem);
+        newItem.classList.add('item');
+    }
+    inputField.value = "";
+}
+
+addBtn.addEventListener('click', addList);
