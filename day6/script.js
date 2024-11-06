c5fa4ca382272cf7392955ee9b06a3bdb6a34471
Siam-al-islam <siamalislam692@gmail.com>
2024-11-06 20:22:51 +0600

    checkbox added before the list name

--- day6/script.js
+++ day6/script.js
@@ -2,31 +2,41 @@ const inputField = document.querySelector('#input-field');
 const form = document.getElementById('form');
 const addBtn = document.querySelector('#add-btn');
 const itemContainer = document.querySelector('#list-container');
-const item = document.querySelector('.item');
 
 function handleForm(event) { event.preventDefault(); }
 form.addEventListener('submit', handleForm);
 
 const addList = () => {
-    if (inputField.value == "") {
-        alert("please add something");
-    }
-    else {
-        const newItem = document.createElement('p');
+    if (inputField.value === "") {
+        alert("Please add something");
+    } else {
+        const newItem = document.createElement('div');
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.id = 'checkbox';
+
+        const itemText = document.createElement('p');
+        itemText.classList.add('list-title');
+        itemText.innerText = inputField.value;
+
         const span = document.createElement('span');
         span.innerHTML = '&#10006;';
-        newItem.innerText = inputField.value;
-        itemContainer.appendChild(newItem);
+
+        newItem.appendChild(checkbox);
+        newItem.appendChild(itemText);
         newItem.appendChild(span);
+
         newItem.classList.add('item');
+        itemContainer.appendChild(newItem);
+
+        inputField.value = "";
     }
-    inputField.value = "";
-}
+};
 
 itemContainer.addEventListener("click", (e) => {
     if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
     }
-})
+});
 
 addBtn.addEventListener('click', addList);
