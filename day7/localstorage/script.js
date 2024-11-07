89fede24cf0773043af8e8180909ce8581ebb6c8
Siam-al-islam <siamalislam692@gmail.com>
2024-11-07 22:03:18 +0600

    added functionallity for button click and add list

--- day7/localstorage/script.js
+++ day7/localstorage/script.js
@@ -4,3 +4,16 @@ const handleForm = (e) => {
     e.preventDefault();
 }
 form.addEventListener('submit', handleForm);
+
+const inputField = document.querySelector("#input-field");
+const listContainer = document.querySelector("#list-container");
+const addBtn = document.querySelector("#add-btn");
+
+const addList = () => {
+    const list = document.createElement('li');
+    list.innerText = inputField.value;
+    listContainer.appendChild(list);
+    inputField.value = "";
+}
+
+addBtn.addEventListener('click', addList);
