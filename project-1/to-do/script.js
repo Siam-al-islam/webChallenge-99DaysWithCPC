1ecf0319eb69c1be8cfa1d35aea961032403bde9
Siam-al-islam <siamalislam692@gmail.com>
2024-11-09 21:41:58 +0600

    list wrapper style added

--- project-1/to-do/script.js
+++ project-1/to-do/script.js
@@ -6,10 +6,37 @@ form.addEventListener("click", (e) => {
 
 const inputField = document.querySelector("#input-field");
 const addBtn = document.querySelector("#addBtn");
+let errorMsg = document.querySelector("#error");
+const listWrapper = document.querySelector("#list-wrapper");
 
 const addList = () => {
     if (inputField.value == "") {
-        document.querySelector("#error").innerHTML = "Please write a task";
+        errorMsg.innerHTML = "Please write a task";
+    }
+    else {
+        errorMsg.innerHTML = "";
+
+        const itemWrapper = document.createElement("div");
+        itemWrapper.classList.add("item-wrapper");
+
+        const checkBox = document.createElement("div");
+        checkBox.classList.add("check-box");
+
+        const remove = document.createElement("i");
+        const removeClassesToAdd = ['fa', 'fa-regular', 'fa-trash'];
+        remove.classList.add(...removeClassesToAdd);
+
+        const li = document.createElement("li");
+        li.innerText = inputField.value;
+        li.classList.add("list");
+
+        listWrapper.appendChild(itemWrapper)
+
+        itemWrapper.appendChild(checkBox);
+        itemWrapper.appendChild(li);
+        itemWrapper.appendChild(remove);
+
+        inputField.value = "";
     }
 }
 
