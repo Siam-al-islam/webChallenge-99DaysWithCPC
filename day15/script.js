3fc376f233d879d9178e96590ad74b78c33bf4f9
Siam-al-islam <siamalislam692@gmail.com>
2024-11-15 20:47:16 +0600

    making interative button

---
+++ day15/script.js
@@ -0,0 +1,16 @@
+const btn100 = document.querySelector("#btn100");
+const btn300 = document.querySelector("#btn300");
+const btn500 = document.querySelector("#btn500");
+const text = document.querySelector("#text");
+
+const addHundrade = () => {
+    text.innerHTML = 200;
+}
+
+const addThreeHundrade = () => {
+    text.innerHTML = 300;
+}
+const addFiveHundrade = () => {
+    text.innerHTML = 500;
+}
+
