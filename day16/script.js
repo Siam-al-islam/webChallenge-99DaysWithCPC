0fe86d471ab0301e284d0e8be76a773bfdb1c36f
Siam-al-islam <siamalislam692@gmail.com>
2024-11-16 21:57:03 +0600

    added numbers by different loops

--- day16/script.js
+++ day16/script.js
@@ -1,2 +1,17 @@
 const number = document.getElementById("number");
-console.log(number);
+const number2 = document.getElementById("number2");
+
+let sum = 0;
+
+for (let i = 1; i <= 10; i++) {
+    sum += i;
+    number.innerText = sum;
+}
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let sum2 = 0;
+
+array.forEach((i) => {
+    sum2 += i;
+    number2.innerText = sum2;
+})
