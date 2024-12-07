24421dca88f677856749b9d9f607a39a8781a2ac
Siam-al-islam <siamalislam692@gmail.com>
2024-12-07 18:37:37 +0600

    added js file

---
+++ project-4/Foodyfy/js/script.js
@@ -0,0 +1,7 @@
+const searchField = document.querySelector("#search-navbar");
+const cardContainer = document.querySelector("#card-container");
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+})
