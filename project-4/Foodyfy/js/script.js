8b83e9224e5897a1b271de0a0275203823cd8e26
Siam-al-islam <siamalislam692@gmail.com>
2024-12-08 20:50:20 +0600

    fixes search problem

--- project-4/Foodyfy/js/script.js
+++ project-4/Foodyfy/js/script.js
@@ -1,6 +1,6 @@
 const searchField = document.querySelector("#search-navbar");
 const cardContainer = document.querySelector("#card-container");
-const form = document.getElementById("form");
+const forms = document.querySelectorAll("#form");
 const container = document.querySelector("#container");
 
 const getRecipes = async (query) => {
@@ -8,52 +8,57 @@ const getRecipes = async (query) => {
     const res = await data.json();
     console.log(res);
 
-    res.meals.forEach(meal => {
-        const htmlStr = `
-        <div class="max-w-[485px] rounded-2xl shadow-2xl pb-8">
-            <img class="rounded-t-2xl h-[333px] w-full object-cover" src="${meal.strMealThumb ? meal.strMealThumb : "https://i.ibb.co.com/KG5kHnx/image-not-found-scaled.png"}"
-            alt="">
-            <div class="flex items-center justify-between mt-4 px-6">
-                <div>
-                    <h2 class="text-xl md:text-3xl font-bold">${meal.strMeal.substring(0, 15) + "..."}</h2>
-                    <button
-                        class="bg-[#F48E28] font-semibold px-5 md:px-7 py-2 rounded-full text-white mt-4">Add To
-                        Cart
-                    </button>
+    if (res) {
+        res.meals.forEach(meal => {
+            const htmlStr = `
+            <div class="max-w-[485px] rounded-2xl shadow-2xl pb-8">
+                <img class="rounded-t-2xl h-[333px] w-full object-cover" src="${meal.strMealThumb ? meal.strMealThumb : "https://i.ibb.co.com/KG5kHnx/image-not-found-scaled.png"}"
+                alt="">
+                <div class="flex items-center justify-between mt-4 px-6">
+                    <div>
+                        <h2 class="text-xl md:text-3xl font-bold">${meal.strMeal.substring(0, 15) + "..."}</h2>
+                        <button
+                            class="bg-[#F48E28] font-semibold px-5 md:px-7 py-2 rounded-full text-white mt-4">Add To
+                            Cart
+                        </button>
+                    </div>
+                    <div>
+                        <div class="flex items-center gap-2">
+                            <svg width="14" height="13" viewBox="0 0 14 13" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M7 1L8.854 4.6204L13 5.20452L10 8.02103L10.708 12L7 10.1204L3.292 12L4 8.02103L1 5.20452L5.146 4.6204L7 1Z"
+                                    fill="#F48E28" stroke="#F48E28" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round" />
+                            </svg>
+                            <span>4.9</span>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <div class="flex items-center gap-2">
-                        <svg width="14" height="13" viewBox="0 0 14 13" fill="none"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M7 1L8.854 4.6204L13 5.20452L10 8.02103L10.708 12L7 10.1204L3.292 12L4 8.02103L1 5.20452L5.146 4.6204L7 1Z"
-                                fill="#F48E28" stroke="#F48E28" stroke-width="2" stroke-linecap="round"
-                                stroke-linejoin="round" />
-                        </svg>
-                        <span>4.9</span>
+                <div class="px-6 mt-8">
+                    <h2 class="font-medium">Ingredients:</h2>
+                    <div class="flex gap-2 flex-wrap mt-3">
+                        <p class="text-xs font-semibold bg-orange-200 px-2 py-1 rounded-xl">${meal.strIngredient1}</p>
+                        <p class="text-xs font-semibold bg-orange-200 px-2 py-1 rounded-xl">${meal.strIngredient2}</p>
+                        <p class="text-xs font-semibold bg-orange-200 px-2 py-1 rounded-xl">${meal.strIngredient3}</p>
+                        <p class="text-xs font-semibold bg-orange-200 px-2 py-1 rounded-xl">${meal.strIngredient4}</p>
+                        <p class="text-xs font-semibold bg-orange-200 px-2 py-1 rounded-xl">${meal.strIngredient5}</p>
                     </div>
                 </div>
             </div>
-            <div class="px-6 mt-8">
-                <h2 class="font-medium">Ingredients:</h2>
-                <div class="flex gap-2 flex-wrap mt-3">
-                    <p class="text-xs font-semibold bg-orange-200 px-2 py-1 rounded-xl">${meal.strIngredient1}</p>
-                    <p class="text-xs font-semibold bg-orange-200 px-2 py-1 rounded-xl">${meal.strIngredient2}</p>
-                    <p class="text-xs font-semibold bg-orange-200 px-2 py-1 rounded-xl">${meal.strIngredient3}</p>
-                    <p class="text-xs font-semibold bg-orange-200 px-2 py-1 rounded-xl">${meal.strIngredient4}</p>
-                    <p class="text-xs font-semibold bg-orange-200 px-2 py-1 rounded-xl">${meal.strIngredient5}</p>
-                </div>
-            </div>
-        </div>
-        `;
-        cardContainer.insertAdjacentHTML('afterbegin', htmlStr);
-    });
+            `;
+            cardContainer.insertAdjacentHTML('afterbegin', htmlStr);
+        });
+    }
+
 }
 
-form.addEventListener("submit", (e) => {
-    cardContainer.innerHTML = '';
-    e.preventDefault();
-    const searchInput = searchField.value.trim();
-    getRecipes(searchInput);
-    container.classList.add('hidden');
+forms.forEach(form => {
+    form.addEventListener("submit", (e) => {
+        cardContainer.innerHTML = '';
+        e.preventDefault();
+        const searchInput = searchField.value.trim();
+        getRecipes(searchInput);
+        container.classList.add('hidden');
+    })
 })
