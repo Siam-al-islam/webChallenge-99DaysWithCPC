2b1b02e5315b5e2ed7dfb93810023377211e7fea
Siam-al-islam <siamalislam692@gmail.com>
2024-12-19 19:55:34 +0600

    added cart sytem updated

--- project-5/WristVault/js/script.js
+++ project-5/WristVault/js/script.js
@@ -3,6 +3,7 @@ const cartTab = document.querySelector("#cartTab");
 const cartContent = document.querySelector("#cartContent");
 const cartIcon = document.querySelector("#cartIcon");
 const closeBtn = document.querySelector("#close");
+const badgeText = document.querySelector("#cart-text");
 let cartList = [];
 
 const getWatches = async () => {
@@ -29,14 +30,34 @@ const getWatches = async () => {
                     </div>
                 </div>
                 <h3 class="font-semibold mt-3">Brand: <span class="text-[#AE946A]">${watch.brand}</span></h3>
-                <div class="flex gap-2">
-                    <button class="mt-4 w-2/4 bg-[#AE946A] text-white py-2 rounded-md add-to-cart-btn" data-index="${index}">Add To Cart</button>
-                    <button class="mt-4 w-2/4 bg-[#AE946A] text-white py-2 rounded-md">View Details</button>
+                <div class="flex gap-2 items-center">
+                    <button class="mt-4 w-2/4 bg-[#AE946A] text-white py-2 rounded-md add-to-cart-btn" data-index="${index}" type="button">
+                        Add to cart
+                    </button>
+                    <div id="popup-modal-${index}" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                        <div class="relative p-4 w-full max-w-md max-h-full">
+                            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                            <button type="button" class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal-${index}">
+                                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                </svg>
+                                <span class="sr-only">Close modal</span>
+                            </button>
+                            <div class="p-4 md:p-5 text-center">
+                                <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Add to cart this product?</h3>
+                                <button id="sure-btn" data-modal-hide="popup-modal-${index}" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+                                    Yes, I'm sure
+                                </button>
+                                <button data-modal-hide="popup-modal-${index}" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">No, cancel</button>
+                            </div>
+                        </div>
+                    </div>                   
+                </div>
+                <button class="mt-4 w-2/4 bg-[#AE946A] text-white py-2 rounded-md add-to-cart-btn">View More</button>
                 </div>
             </div>
         </div>
         `;
-
         cardWrapper.insertAdjacentHTML('beforeend', cardStr);
     });
 
@@ -44,16 +65,14 @@ const getWatches = async () => {
         button.addEventListener('click', (e) => {
             const index = e.currentTarget.dataset.index;
             const selectedWatch = res.watches[index];
-            addToCart(selectedWatch);
+            addToCart(selectedWatch, index);
         });
     });
 };
 
-getWatches();
-
-const addToCart = (item) => {
+const addToCart = (item, index) => {
     cartList.push(item);
-
+    badgeText.innerHTML = cartList.length;
     const cartItemStr = `
         <div class="flex items-center justify-between gap-3 mt-8">
             <img class="w-11 object-cover"
@@ -63,14 +82,28 @@ const addToCart = (item) => {
             <span>$ <span>${item.price}</span></span>
         </div>
     `;
-
     cartContent.insertAdjacentHTML('beforeend', cartItemStr);
-}
+
+    const modal = document.querySelector(`#popup-modal-${index}`);
+    modal.classList.remove("hidden");
+    modal.classList.add("flex");
+};
+
+document.addEventListener('click', (e) => {
+    if (e.target.matches('[data-modal-hide]')) {
+        const modalId = e.target.getAttribute('data-modal-hide');
+        const modal = document.querySelector(`#${modalId}`);
+        modal.classList.add("hidden");
+        modal.classList.remove("flex");
+    }
+});
 
 cartIcon.addEventListener('click', () => {
     cartTab.classList.toggle("hidden");
-})
+});
 
 closeBtn.addEventListener('click', () => {
     cartTab.classList.add("hidden");
-})
+});
+
+getWatches();
