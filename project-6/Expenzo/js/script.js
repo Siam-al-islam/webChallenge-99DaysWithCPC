a1a1bcdcfe3842babda86c7db1ada4e7455a1500
Siam Al Islam <77141415+Siam-al-islam@users.noreply.github.com>
2024-12-26 15:47:09 +0600

    updated record and back button issues

--- project-6/Expenzo/js/script.js
+++ project-6/Expenzo/js/script.js
@@ -1,6 +1,16 @@
 const recordBtn = document.querySelector("#record-btn");
+const backBtn = document.querySelector("#back-btn");
 const welcomeSection = document.querySelector("#welcome-sec");
+const expenseDetails = document.querySelector("#expense-details");
 
-recordBtn.addEventListener("click", ()=>{
+const handleRecordBtn = () => {
+    showElement(backBtn, "hidden");
     hideElement(welcomeSection, "hidden");
-})
+    showElement(expenseDetails, "hidden");
+};
+
+const handleBackBtn = () => {
+    hideElement(expenseDetails, "hidden");
+    hideElement(backBtn, "hidden");
+    showElement(welcomeSection, "hidden");
+}
--- project-6/Expenzo/js/utility.js
+++ project-6/Expenzo/js/utility.js
@@ -1,3 +1,7 @@
 const hideElement = (element, className) => {
     element.classList.add(className);
 }
+
+const showElement = (element, className) => {
+    element.classList.remove(className);
+}
