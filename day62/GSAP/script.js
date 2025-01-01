46c38f75190525eb07255706aff53c2840ae7088
Siam Al Islam <77141415+Siam-al-islam@users.noreply.github.com>
2025-01-01 20:40:51 +0600

    introducing to GSAP

---
+++ day62/GSAP/script.js
@@ -0,0 +1,17 @@
+gsap.from("#box", {
+    x:400,
+    duration:2,
+    delay:1,
+    rotate:360,
+    backgroundColor:"blue",
+    borderRadius:"50%",
+    scale:0.5
+})
+
+gsap.to("#box2", {
+    x:400,
+    duration:2,
+    delay:1,
+    borderRadius:"50%",
+    scale:0.5
+})
