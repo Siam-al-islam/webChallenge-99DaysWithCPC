f7e2db46ee6dd093c63818aad61c81ebca45f5ab
Siam Al Islam <77141415+Siam-al-islam@users.noreply.github.com>
2025-01-07 19:12:12 +0600

    added about me section

--- project-7/Portfolio-by-GSAP/script.js
+++ project-7/Portfolio-by-GSAP/script.js
@@ -1,38 +1,51 @@
 let tl = gsap.timeline();
 
-tl.from(".profile-banner .text-1", {
+tl.from(".banner .text-1", {
     x:100,
     opacity: 0,
     delay: 1,
     duration: .3
 })
 
-tl.from(".profile-banner .text-2", {
+tl.from(".banner .text-2", {
     scale: 0,
     opacity: 0,
     duration: .5
 })
 
-tl.from(".profile-banner .text-3", {
+tl.from(".banner .text-3", {
     x:100,
     opacity: 0,
     duration: .5
 })
 
-tl.from(".profile-banner p", {
+tl.from(".banner p", {
     x:-100,
     opacity: 0,
     duration: .5
 })
 
-gsap.to(".profile-banner img", {
-    y: 100,
+gsap.to(".banner img", {
+    y: -50,
     duration: 2,
+    stagger: 0.5,
     repeat: -1
 })
 
-gsap.from(".profile-banner img", {
-    y:-50,
+gsap.from(".banner img", {
+    y:50,
     duration: 2,
-    repeat: -1
+    repeat: -1,
+    stagger: 0.5
+})
+
+gsap.to("body", {
+    backgroundColor: "black",
+    scrollTrigger: {
+        trigger:".about-wrapper",
+        scrolller: "body",
+        start: "top 50%",
+        scrub: 1,
+        end:"bottom 100%",
+    }
 })
