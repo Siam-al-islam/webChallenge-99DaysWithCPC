1600e7c2e9c18323eebcecd61397645ecae6d945
Siam-al-islam <siamalislam692@gmail.com>
2025-01-17 19:24:05 +0600

    home page full design implemented

---
+++ project-8/Bangladesh Railway/js/script.js
@@ -0,0 +1,7 @@
+// navbar responsive
+const navList = document.querySelector(".navbar-list");
+const navIcon = document.querySelector(".navbar-icon");
+
+navIcon.addEventListener('click', () => {
+    navList.classList.toggle("hidden");
+})
