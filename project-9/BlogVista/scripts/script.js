b42c6719ffc993ad4f083816bfde28f91e87f1dc
Siam-al-islam <siamalislam692@gmail.com>
2025-01-21 18:35:59 +0600

    added responsive javascript code for navbar and fixed overflow issue

---
+++ project-9/BlogVista/scripts/script.js
@@ -0,0 +1,9 @@
+// navbar responsive
+const navList = document.querySelector(".navbar-list");
+const navIcon = document.querySelector(".navbar-icon");
+
+navIcon.addEventListener('click', () => {
+    navList.classList.toggle("hidden");
+})
+
+//
