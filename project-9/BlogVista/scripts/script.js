8c3c115f46db423d7700ba9237d501c410eff1fa
Siam-al-islam <siamalislam692@gmail.com>
2025-01-21 20:45:41 +0600

    added read more btn functionality by using modal

--- project-9/BlogVista/scripts/script.js
+++ project-9/BlogVista/scripts/script.js
@@ -8,6 +8,9 @@ navIcon.addEventListener('click', () => {
 
 //showing blogs to the UI
 const blogsContainer = document.querySelector("#latest-posts-container");
+const modal = document.querySelector("#blog-modal");
+const closeModal = document.querySelector("#close-modal");
+const blogContent = document.querySelector("#blog-content");
 
 const showBlogs = async () => {
     const res = await fetch("blogs.json");
@@ -15,7 +18,7 @@ const showBlogs = async () => {
     console.log(data);
 
     if (data) {
-        data.forEach(blog => {
+        data.forEach((blog, index) => {
             const blogsStr = `
                 <div class="border border-[#E8E8EA] rounded-xl p-4 w-full">
                     <div>
@@ -28,7 +31,7 @@ const showBlogs = async () => {
                         <h2 class="mt-4 font-semibold text-2xl">
                             ${blog.blog_title}
                         </h2>
-                        <button class="mt-4 w-full text-white font-bold py-2 rounded-xl bg-[#000000]">Read More</button>
+                        <button data-index="${index}" id="read-more-btn" class="mt-4 w-full text-white font-bold py-2 rounded-xl bg-[#000000]">Read More</button>
                         <div class="flex items-center gap-5 text-[#97989F] mt-6">
                             <div class="flex items-center gap-3">
                                 <img src="${blog.author_profile}" alt="">
@@ -44,7 +47,39 @@ const showBlogs = async () => {
 
             blogsContainer.insertAdjacentHTML("beforeend", blogsStr);
         });
+
+        document.querySelectorAll("#read-more-btn").forEach(btn => {
+            btn.addEventListener('click', (e) => {
+                const blogIndex = e.target.dataset.index;
+                const blog = data[blogIndex];
+
+                blogContent.innerHTML = `
+                <div>
+                    <img class="w-full mt-6" src="${blog.image}" alt="">
+                    <h3 class="text-[#4B6BFB] bg-[#4b6bfb10] px-3 py-1 rounded-md w-fit font-medium mt-4">
+                            ${blog.category}
+                    </h3>
+                    <h2 class="mt-5 font-semibold text-2xl">${blog.blog_title}</h2>
+                    <p class="mt-3">${blog.blog_description}</p>
+                    <div class="flex items-center justify-between">
+                        <div class="flex items-center gap-3 mt-5">
+                            <img class="w-8 h-8 rounded-full" src="${blog.author_profile}" alt="">
+                            <h3>${blog.author_name}</h3>
+                        </div>
+                        <h4 class="text-[#97989F] mt-2">${blog.upload_date}</h4>
+                    </div>
+                </div>
+                `;
+
+                modal.classList.remove("hidden");
+            })
+        })
     }
 }
+
+closeModal.addEventListener("click", () => {
+    modal.classList.add("hidden");
+});
+
 
 showBlogs()
